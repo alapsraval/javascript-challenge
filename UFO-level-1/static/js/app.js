790ab04947cfaba5313779e25e93646c36f4595b
alapraval <alapsraval@gmail.com>
2020-04-08 20:29:57 -0500

    UFO Level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,30 @@
 // from data.js
-var tableData = data;
+var UFOData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("#ufo-table tbody");
+
+UFOData.forEach((UFO) => {
+    var row = tbody.append("tr");
+    Object.entries(UFO).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+var inputElement = d3.select("#datetime");
+
+button.on("click", function () {
+    var inputValue = inputElement.property("value");
+    var filteredData = UFOData.filter(UFOData => UFOData.datetime === inputValue);
+    tbody.selectAll("tr").remove();
+    filteredData.forEach((UFO) => {
+        var row = tbody.append("tr");
+        Object.entries(UFO).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
